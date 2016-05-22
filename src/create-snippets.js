f5c02e1e1969c08149675db5f8bbf83482717868
Tom Philbin <t.philbin@gmail.com>
2016-05-22 18:11:39 +0100

    Stopped changing the snippet prefix, just using the function name now.

--- src/create-snippets.js
+++ src/create-snippets.js
@@ -1,10 +1,9 @@
 const _ = require('lodash');
-const createSnippetPrefix = require('./create-snippet-prefix');
 const createSnippetParams = require('./create-snippet-params');
 
 const createSnippets = (data) => _.reduce(data, (result, value) => {
     result[value.description] = {
-        prefix: createSnippetPrefix(value.name),
+        prefix: value.name,
         body: [
             `${value.longname}(${createSnippetParams(value.params)});`
         ]
