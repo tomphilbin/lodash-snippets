9bd86fba51f2f0afdad410acea6b374b1d174102
Tom Philbin <tomphilbin@enterprise.local>
2017-02-12 19:59:57 +0000

    Refactored function call to variable.

--- src/create-snippets.js
+++ src/create-snippets.js
@@ -3,11 +3,12 @@ import createSnippetParams from './create-snippet-params';
 
 const createSnippets = data => _.reduce(data, (result, value) => {
   const newResult = result;
+  const params = createSnippetParams(value.params);
 
   newResult[value.description] = {
     prefix: value.name,
     body: [
-      `${value.longname}(${createSnippetParams(value.params)})`,
+      `${value.longname}(${params})`,
     ],
   };
 
